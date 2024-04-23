8547eb8322d2b7b19fea468a06014c96239e17ee
Mel/ <tchoikuemel06@gmail.com>
2024-04-23 13:38:51 +0100

    add search contact fontionalitie

--- src/Routes/Root.jsx
+++ src/Routes/Root.jsx
@@ -5,37 +5,58 @@ import {
 	Form,
 	redirect,
 	useNavigation,
+	useSubmit,
 } from 'react-router-dom';
 import { getContacts, createContact } from '../Contacts';
+import { useEffect } from 'react';
 
 export const action = async () => {
 	const contact = await createContact();
 	return redirect(`/contacts/${contact.id}/edit`);
 };
 
-export const loader = async () => {
-	const contacts = await getContacts();
-	return { contacts };
+export const loader = async ({ request }) => {
+	let contacts = await getContacts();
+	const url = new URL(request.url);
+	const q = url.searchParams.get('q');
+	contacts = await getContacts(q);
+	return { contacts, q };
 };
+
 const Root = () => {
-	const { contacts } = useLoaderData();
+	const { contacts, q } = useLoaderData();
 	const navigation = useNavigation();
+	const submit = useSubmit();
+	const searching =
+		navigation.location &&
+		new URLSearchParams(navigation.location.search).has('q');
+	useEffect(() => {
+		document.getElementById('q').value = q;
+	}, [q]);
 	return (
 		<>
 			<div id='sidebar'>
 				<h1>Contacts</h1>
 				<div>
-					<form id='search-form' role='search'>
+					<Form id='search-form' role='search'>
 						<input
 							id='q'
+							className={searching ? 'loading' : ''}
 							aria-label='Search contacts'
 							placeholder='Search'
 							type='search'
 							name='q'
+							defaultValue={q}
+							onChange={(event) => {
+                const isFirstSearch = q == null;
+                submit(event.currentTarget.form, {
+                  replace: !isFirstSearch,
+                });
+							}}
 						/>
-						<div id='search-spinner' aria-hidden hidden={true} />
+						<div id='search-spinner' aria-hidden hidden={!searching} />
 						<div className='sr-only' aria-live='polite'></div>
-					</form>
+					</Form>
 					<Form method='post'>
 						<button type='submit'>New</button>
 					</Form>
